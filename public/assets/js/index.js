4a2aa9fabad8d9d663aad05960e3e073c0486523
AJoelj84 <ajoelj84@gmail.com>
2023-05-15 08:45:41 -0400

    added javascript code to program, testing and editing as needed

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -25,6 +25,28 @@ const hide = (elem) => {
 // activeNote is used to keep track of the note in the textarea
 let activeNote = {};
 
+
+
+const getNoteById = (id) =>
+  fetch(`/api/notes/${id}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
 const getNotes = () =>
   fetch('/api/notes', {
     method: 'GET',
@@ -33,38 +55,45 @@ const getNotes = () =>
     },
   });
 
-const saveNote = (note) =>
-  fetch('/api/notes', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(note),
-  });
+  const saveNote = (note) => {
+    if (note.id) {
+      // Existing note, perform update
+      return fetch(`/api/notes/${note.id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(note),
+      });
+    } else {
+      // New note, perform creation
+      return fetch('/api/notes', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(note),
+      });
+    }
+  };
+  
 
-const deleteNote = (id) =>
-  fetch(`/api/notes/${id}`, {
-    method: 'DELETE',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-
-const renderActiveNote = () => {
-  hide(saveNoteBtn);
-
-  if (activeNote.id) {
-    noteTitle.setAttribute('readonly', true);
-    noteText.setAttribute('readonly', true);
-    noteTitle.value = activeNote.title;
-    noteText.value = activeNote.text;
-  } else {
-    noteTitle.removeAttribute('readonly');
-    noteText.removeAttribute('readonly');
-    noteTitle.value = '';
-    noteText.value = '';
-  }
-};
+  const renderActiveNote = () => {
+    show(saveNoteBtn);
+  
+    if (activeNote.id) {
+      noteTitle.removeAttribute('readonly');
+      noteText.removeAttribute('readonly');
+      noteTitle.value = activeNote.title;
+      noteText.value = activeNote.text;
+    } else {
+      noteTitle.setAttribute('readonly', true);
+      noteText.setAttribute('readonly', true);
+      noteTitle.value = '';
+      noteText.value = '';
+    }
+  };
+  
 
 const handleNoteSave = () => {
   const newNote = {
@@ -79,28 +108,51 @@ const handleNoteSave = () => {
 
 // Delete the clicked note
 const handleNoteDelete = (e) => {
-  // Prevents the click listener for the list from being called when the button inside of it is clicked
   e.stopPropagation();
-
-  const note = e.target;
-  const noteId = JSON.parse(note.parentElement.getAttribute('data-note')).id;
-
-  if (activeNote.id === noteId) {
-    activeNote = {};
-  }
-
-  deleteNote(noteId).then(() => {
-    getAndRenderNotes();
-    renderActiveNote();
-  });
+  const note = e.target.parentElement;
+  const noteId = note.dataset.noteId;
+  deleteNoteById(noteId)
+    .then(() => {
+      if (activeNote.id === noteId) {
+        activeNote = {};
+        renderActiveNote();
+      }
+      getAndRenderNotes();
+    })
+    .catch((error) => {
+      console.error(error);
+      // Handle error here
+    });
 };
+
+
+
+const deleteNoteById = (id) =>
+  fetch(`/api/notes/${id}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
 
 // Sets the activeNote and displays it
 const handleNoteView = (e) => {
   e.preventDefault();
-  activeNote = JSON.parse(e.target.parentElement.getAttribute('data-note'));
-  renderActiveNote();
+  const note = e.target.parentElement;
+  const noteId = JSON.parse(note.getAttribute('data-note')).note_id;
+  getNoteById(noteId)
+    .then((response) => response.json())
+    .then((data) => {
+      activeNote = data;
+      renderActiveNote();
+    })
+    .catch((error) => {
+      console.error(error);
+      // Handle error here
+    });
 };
+
 
 // Sets the activeNote to and empty object and allows the user to enter a new note
 const handleNewNoteView = (e) => {
@@ -120,13 +172,32 @@ const handleRenderSaveBtn = () => {
 const renderNoteList = async (notes) => {
   let jsonNotes = await notes.json();
   if (window.location.pathname === '/notes') {
-    noteList.forEach((el) => (el.innerHTML = ''));
+    noteList.innerHTML = ''; // Clear the note list container
   }
 
   let noteListItems = [];
 
+  if (jsonNotes.length === 0) {
+    noteListItems.push(createLi('No saved Notes', false));
+  }
+
+  jsonNotes.forEach((note) => {
+    const li = createLi(note.title);
+    li.dataset.noteId = note.note_id; // Use 'note_id' instead of 'id'
+
+    noteListItems.push(li);
+  });
+
+  if (window.location.pathname === '/notes') {
+    noteListItems.forEach((note) => noteList.append(note));
+  }
+};
+
+
+  let noteListItems = [];
+
   // Returns HTML element with or without a delete button
-  const createLi = (text, delBtn = true) => {
+  const createLi = (text, delBtn = true, noteId) => {
     const liEl = document.createElement('li');
     liEl.classList.add('list-group-item');
 
@@ -151,6 +222,8 @@ const renderNoteList = async (notes) => {
       liEl.append(delBtnEl);
     }
 
+    liEl.dataset.noteId = noteId; // Set the note ID as a dataset attribute
+
     return liEl;
   };
 
@@ -159,16 +232,17 @@ const renderNoteList = async (notes) => {
   }
 
   jsonNotes.forEach((note) => {
-    const li = createLi(note.title);
-    li.dataset.note = JSON.stringify(note);
-
+    const li = createLi(note.title, true, note.note_id);
     noteListItems.push(li);
   });
 
   if (window.location.pathname === '/notes') {
-    noteListItems.forEach((note) => noteList[0].append(note));
+    noteListItems.forEach((note) => noteList[0].append(note)); // Append note items to the list
   }
-};
+
+
+// Rest of the code...
+
 
 // Gets notes from the db and renders them to the sidebar
 const getAndRenderNotes = () => getNotes().then(renderNoteList);
--- routes/notes.js
+++ routes/notes.js
@@ -1,5 +1,6 @@
 const notes = require('express').Router();
-const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const { readFromFile, writeToFile, readAndAppend } = require('../helpers/fsUtils');
+
 const uuid = require('../helpers/uuid');
 
 console.log("notes running");
@@ -27,22 +28,48 @@ notes.post('/', (req, res) => {
     }
   });
 
-  notes.delete('/', (req, res) => {
-    console.info(`${req.method} request received to add a tip`);
-  
-    const { title, text } = req.body;
-  
-    if (req.body) {
-      const newNote = {
-        title,
-        text
-      };
-  
-      readAndAppend(newNote, './db/db.json');
-      res.json(`Note added successfully 🚀`);
-    } else {
-      res.error('Error in adding note');
-    }
-  });
 
+  notes.get('/:id', (req, res) => {
+    console.info(`${req.method} request received for a single note`);
+    const noteId = req.params.id;
+    readFromFile('./db/db.json')
+      .then((data) => {
+        const notes = JSON.parse(data);
+        const note = notes.find((note) => note.note_id === noteId);
+        if (note) {
+          res.json(note);
+        } else {
+          res.status(404).json({ message: 'Note not found' });
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).json({ error: 'Internal server error' });
+      });
+  });
+  
+
+
+
+
+
+
+  notes.delete('/:id', (req, res) => {
+    console.info(`${req.method} request received to delete a note`);
+    const noteId = req.params.id;
+    readFromFile('./db/db.json')
+      .then((data) => {
+        let notes = JSON.parse(data);
+        notes = notes.filter((note) => note.note_id !== noteId);
+        writeToFile('./db/db.json', notes);
+        res.json('Note deleted successfully');
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).json({ error: 'Internal server error' });
+      });
+  });
+  
+
+  
 module.exports = notes;
